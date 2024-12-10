3f4254d92ab987def0b46fb5f04e71009331ef78
Asadulnyc <asadulk@nycstudents.net>
2024-12-10 01:10:34 +0000

    333 Done

--- Main.java
+++ Main.java
@@ -5,44 +5,68 @@ import java.io.FileNotFoundException;
 public class Main {
     private static File f;
     private static Scanner s;
+
     public static void main(String[] args) throws FileNotFoundException {
         f = new File("words.txt");
         int twoLetter = twoLetter();
-        //int longWord = longWord();
-        //int palindrome = palindrome();
+        int longWord = longWord();
+        int palindrome = palindromeCount();
         System.out.println("Number of Two Letter Words: " + twoLetter);
-       // System.out.println("Number of Tied Longest Words: " + longWord);
-        //System.out.println("Number of Palindromes: " + palindrome);*/
+        System.out.println("Number of Tied Longest Words: " + longWord);
+        System.out.println("Number of Palindromes: " + palindrome);
         s.close();
     }
 
-    public static int twoLetter() throws FileNotFoundException{
+    public static int twoLetter() throws FileNotFoundException {
         s = new Scanner(f);
         int twoLetter = 0;
         while (s.hasNext()) {
-            if (((s.nextLine()).length()) == 2)
+            String line = s.nextLine();
+            if (((line).length()) == 2)
                 twoLetter++;
         }
         return twoLetter;
     }
-    
-    /*public static int longWord() throws FileNotFoundException{
+
+    public static int longWord() throws FileNotFoundException {
         s = new Scanner(f);
-        int evens = 0;
+        int longest = 0;
         while (s.hasNext()) {
-            if (s.nextInt() % 2 == 0)
-                evens++;
+            String line = s.nextLine();
+            if (line.length() > longest)
+                longest = line.length();
         }
-        return evens;
+        int numLong = 0;
+        Scanner d = new Scanner(f);
+        while (d.hasNext()) {
+            String fine = d.nextLine();
+            if (fine.length() == longest) {
+                numLong++;
+            }
+        }
+        d.close();
+        return numLong;
     }
 
-    public static int palindrome() throws FileNotFoundException{
+    public static int palindromeCount() throws FileNotFoundException {
         s = new Scanner(f);
-        int twoDigit = 0;
+        int palindromes = 0;
         while (s.hasNext()) {
-            if (s.nextInt() >= 10 && s.nextInt() <= 99)
-                twoDigit++;
+            String line = s.nextLine();
+            if(isPalindrome(line)) palindromes++;
         }
-        return twoDigit;
-    }*/
+        return palindromes;
+    }
+
+    public static boolean isPalindrome(String word) throws FileNotFoundException {
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
